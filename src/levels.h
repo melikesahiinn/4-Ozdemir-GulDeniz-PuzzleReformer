6558af63cf70a053b672f220a1abd075b8982eea
melikesahiinn <melikeesahiin1@gmail.com>
2026-01-19 01:28:32 +0300

    Update levels.h

--- src/levels.h
+++ src/levels.h
@@ -4,8 +4,12 @@
 struct LevelDef {
     int n = 3;
     int tileSize = 100;
-    std::vector<int> correct; // size n*n, each 0..3
-    std::vector<int> start;   // size n*n, each 0..3
-};
 
+    std::vector<int> correct; 
+    std::vector<int> start;  
+
+    std::vector<int> lockedTiles; /
+    int moveLimit = -1;        
+    bool hintEnabled = true;    
+};
 const std::vector<LevelDef>& GetAllLevels();
